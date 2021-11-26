f3391888bd43fb388e120bf10ca08e66b5b4ebba
S-Keyp <s-keyp@outlook.fr>
2021-11-26 15:27:13 +0100

    Update register.php

--- projet-php/register.php
+++ projet-php/register.php
@@ -6,7 +6,15 @@
 <body>
 <?php
 require('config.php');
-if (isset($_REQUEST['pseudo'], $_REQUEST['email'], $_REQUEST['mdp'])){
+if (isset($_POST['pseudo'], $_POST['email'], $_POST['mdp'])){
+
+	$email= $_POST['email'];
+	$username= $_POST['$username'];
+	$password= $_POST['password'];
+	$request = $conn -> prepare("INSERT into `users` (pseudo, email, mdp) VALUES (:username, :email, :password)");
+	$request -> execute(['username' => $username, 'email' => $email, 'password' => $password]);
+
+
 	// récupérer le nom d'utilisateur et supprimer les antislashes ajoutés par le formulaire
 	$username = stripslashes($_REQUEST['pseudo']);
 	$username = mysqli_real_escape_string($conn, $username); 
@@ -29,15 +37,17 @@ if (isset($_REQUEST['pseudo'], $_REQUEST['email'], $_REQUEST['mdp'])){
     }
 }else{
 ?>
-<form class="box" action="" method="post">
-	<h1 class="box-logo box-title">Bienvenue</a></h1>
-    <h2 class="box-title">S'inscrire</h2>
-	<input type="text" class="box-input" name="pseudo" placeholder="Nom d'utilisateur" required />
-    <input type="text" class="box-input" name="email" placeholder="Email" required />
-    <input type="password" class="box-input" name="mdp" placeholder="Mot de passe" required />
-    <input type="submit" name="submit" value="S'inscrire" class="box-button" />
-    <p class="box-register">Déjà inscrit? <a href="login.php">Connectez-vous ici</a></p>
-</form>
+	<div class="box">
+		<h1 class="box-logo box-title">Bienvenue</a></h1>
+		<h2 class="box-title">S'inscrire</h2>
+		<form action="" method="post">
+			<input type="text" class="box-input" name="pseudo" placeholder="Nom d'utilisateur" required />
+			<input type="text" class="box-input" name="email" placeholder="Email" required />
+			<input type="password" class="box-input" name="mdp" placeholder="Mot de passe" required />
+			<input type="submit" name="submit" value="S'inscrire" class="box-button" />
+			<p class="box-register">Déjà inscrit? <a href="login.php">Connectez-vous ici</a></p>
+		</form>
+	</div>
 <?php } ?>
 </body>
 </html>
