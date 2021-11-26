e366dd6294be7d511a997b8031be4eda1f6d74eb
AdilSab <90918349+AdilSab@users.noreply.github.com>
2021-11-26 15:34:31 +0100

    v2

--- projet-php/config.php
+++ projet-php/config.php
@@ -1,6 +1,4 @@
 <?php
-// Informations d'identification
-
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
@@ -9,9 +7,6 @@ define('DB_NAME', 'projet');
 // Connexion à la base de données MySQL 
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
-// Connexion � la base de donn�es MySQL 
-$conn = new PDO('mysql:host=localhost;dbname=projet;charset=utf8',
-    'root', '', array(PDO::ATTR_PERSISTENT=>true));
 
 // V�rifier la connexion
 if($conn === false){
