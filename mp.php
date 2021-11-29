6a3e6fca1e15c0ba990225e8fbadbf77325aa38b
S-Keyp <s-keyp@outlook.fr>
2021-11-29 16:30:45 +0100

    Update mp.php

--- mp.php
+++ mp.php
@@ -1,26 +1,52 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bg-quizz</title>
-</head>
-<body>
-    <p>qsmdlfjkqsdmlfj</p>
-    <?php
-        require ('projet-php/config.php');
-        $id = rand(1, 2);
-        $reqs = $dbh -> prepare('SELECT * FROM bg_quizz WHERE id= $id');
-        $reqs -> execute();
+<?php
+$titre='bgquizz';
+session_start();
+    echo "<form method='post'>
+    <input type='submit' name='button1'
+            value='Nouvelle question'/>
+    
+    <input type='submit' name='button2'
+            value='Button2'/>
+</form>";
 
-        foreach($reqs as $question) {
-            echo "<h3>$question[question]</h3> 
-            $question[a]
-            $question[b]
-            $question[c]
-            $question[d]";
+    
+    require ('projet-php/config.php');
+    if(isset($_SESSION['ids'])){
+        $ids=$_SESSION['ids'];
+    } else {
+        $ids = [];
+    }
+    $id=2;
+    if(isset($_POST['button1'])) {
+        $max=4;
+        if (count($ids) < $max) {
+            $id = rand(1, $max);
+            while(in_array($id, $ids) ) {
+                $id = rand(1, 4);
+            }
+            $ids[]=$id;    
+            echo "la valeur n'existe pas et à été ajouté";
         }
-    ?>
-</body>
-</html>
+        print_r($ids);   
+        $_SESSION['ids'] = $ids;
+    }
+
+    if(isset($_POST['button2'])) {
+        $_SESSION['ids']=[];
+    }
+    
+    
+    
+    $reqs = $dbh -> prepare("SELECT * FROM bgquizz WHERE id= $id");
+    $reqs -> execute();
+
+    foreach($reqs as $question) {
+        echo "<h3>$question[question]</h3> 
+        $question[a]
+        $question[b]
+        $question[c]
+        $question[d]";
+    }
+
+    
+?>
